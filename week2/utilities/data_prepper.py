4d8962541ff0435562cd371154aeab0732a40225
JT <>
2022-02-27 21:12:23 -0800

    Pass features to XGB for training
    Up until now, the training data being passed to XGBoost was randomly
    generated.  Actually run the generated queries to get the features, and
    use that as the training data.
    
    Go ahead and finish filling out everything that needs implementing in
    data_prepper.py.

--- week2/utilities/data_prepper.py
+++ week2/utilities/data_prepper.py
@@ -232,26 +232,84 @@ class DataPrepper:
         log_queries = lu.create_feature_log_queries(key, query_doc_ids, click_prior_query, self.featureset_name,
                                                     self.ltr_store_name,
                                                     terms_field=terms_field)
-        # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
-        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
-        # Your structure should look like the data frame below
-        feature_results = {}
-        feature_results["doc_id"] = []  # capture the doc id so we can join later
-        feature_results["query_id"] = []  # ^^^
-        feature_results["sku"] = []
-        feature_results["salePrice"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
-            feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  # ^^^
-            feature_results["salePrice"].append(rng.random())
-            feature_results["name_match"].append(rng.random())
+        # query_id appears to be passed as a pandas object.
+        # Extract the value we will actually use.
+        query_id_value = int(query_id.iloc[0])
+
+        docIds          = list()
+        queryIds        = list()
+        skus            = list()
+        feature_results = dict()
+        for query in log_queries:
+            try:
+                response = self.opensearch.search(index=self.index_name, body=query)
+            except RequestError as re:
+                print('Error getting feature weights ({e:s}, q={q:s}).'.format(
+                      e=str(re), q=str(query)))
+                return None
+            # Loop over the hits structure in response and extract out the
+            # features per query_id and doc id.
+            # Also capture and return all query/doc pairs that didn't
+            # return features
+            if (not response):
+                continue
+            hits = response.get('hits', None)
+            if (not hits):
+                continue
+            hits = hits.get('hits', None)
+            if (not hits):
+                continue
+            for (ind, hit) in enumerate(hits):
+                if (not hit):
+                    continue
+                fields = hit.get('fields', None)
+                if (not fields):
+                    continue
+                featureLog = fields.get('_ltrlog', None)
+                if (not featureLog):
+                    continue
+                featureLog = featureLog[0]
+                if (not featureLog):
+                    continue
+                featureLog = featureLog.get('log_entry', None)
+                if (not featureLog):
+                    continue
+
+                sku = hit.get('_source', None)
+                if (sku):
+                    sku = sku.get('sku', None)
+                if (sku):
+                    sku = sku[0]
+                if (sku is None):
+                    sku = -1
+                else:
+                    sku = int(sku)
+
+                docIds  .append(int(hit.get('_id', -1)))
+                queryIds.append(query_id_value)
+                skus    .append(sku)
+
+                addFeatures = (not feature_results)
+                for (ind, feature) in enumerate(featureLog):
+                    featureName = feature.get('name' , None)
+                    if (featureName is None):
+                        continue
+                    if (addFeatures):
+                        featureValues                = list()
+                        feature_results[featureName] = featureValues
+                    else:
+                        # Let this raise KeyError if there is no value.
+                        featureValues = feature_results[featureName]
+                    featureValues.append(feature.get('value', 0))
+
+        if (0 >= len(docIds)):
+            no_results[key] = query_doc_ids
+
+        feature_results['doc_id'  ] = docIds
+        feature_results['query_id'] = queryIds
+        feature_results['sku'     ] = skus
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
-        # IMPLEMENT_END
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
     def normalize_data(self, ranks_features_df, feature_set, normalize_type_map):
@@ -301,5 +359,4 @@ class DataPrepper:
 
     # Determine the number of clicks for this sku given a query (represented by the click group)
     def __num_clicks(self, all_skus_for_query, test_sku):
-        print("IMPLEMENT ME: __num_clicks(): Return how many clicks the given sku received in the set of skus passed ")
-        return 0
+        return (all_skus_for_query == test_sku).sum()
