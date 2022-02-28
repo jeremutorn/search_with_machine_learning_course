6c195f67b22a93fd2230e8a26524be838d539d9f
JT <>
2022-02-27 20:28:07 -0800

    Fix heuristic function and downsampling
    The code for assigning the heuristic and downsampling it was filling in
    random numbers and skipping the downsampling.  Fill in the code to
    properly assign a stepped heuristic as requested in the assignment, and
    select the proper downsampling function.
    
    This would be much simpler as a few if/then constructs, but I wanted to
    try making it capable of handling a much more complicated step function
    efficiently.

--- week2/utilities/click_models.py
+++ week2/utilities/click_models.py
@@ -1,4 +1,5 @@
 # Implements various click models
+import bisect
 import pandas as pd
 import numpy as np
 
@@ -7,12 +8,60 @@ def binary_func(x):
         return 1
     return 0
 
+class Steps(object):
+    '''
+    Contains information on a built-in stepped function, and provides
+    access to that function.
+    '''
+
+    def step(self, x):
+        '''
+        Returns the value of a stepped function evaluated at x.
+        '''
+        ind = bisect.bisect_right(self._xList, x)
+        if (ind < len(self._steps)):
+            return self._steps[ind][1]
+        return self._finalY
+    # End of step().
+
+    _steps = tuple(sorted((
+        (0.05, 0   ),
+        (0.1 , 0.5 ),
+        (0.3,  0.75),
+    )))
+    '''
+    A sorted list of (x, y) pairs:
+      (x0, y0),
+      (x1, y1),
+      ...
+      (xN, yN)
+    The function evalutes as:
+      if (x < x0):
+        return y0
+      elif (x < x1):
+        return y1
+      ...
+      elif (x < xN):
+        return yN
+      return _finalY
+    Using a sorted list allows for faster lookup than linearly iterating.
+    '''
+
+    _finalY = 1
+    '''
+    See description for _steps.
+    '''
+
+    _xList = tuple(x for (x, y) in _steps)
+    '''
+    Just the x values from _steps.
+    '''
+# End of Steps class.
 def step(x):
-    print("IMPLEMENT ME: step(x) a step function with a simple heuristic that buckets grades")
-    return rng.choice([0,0.5, 1.0])
+    return Steps().step(x)
+# End of step().
 
 
-rng = np.random.default_rng(123456)
 # Given a click model type, transform the "grade" into an appropriate value between 0 and 1, inclusive
 # This operates on the data frame and adds a "grade" column
 #
@@ -28,7 +77,8 @@ def apply_click_model(data_frame, click_model_type="binary", downsample=True):
             data_frame = down_sample_continuous(data_frame)
     elif click_model_type == "heuristic":
         data_frame["grade"] = (data_frame["clicks"]/data_frame["num_impressions"]).fillna(0).apply(lambda x: step(x))
-        print("IMPLEMENT ME: apply_click_model(): downsampling")
+        if downsample:
+            data_frame = down_sample_buckets(data_frame)
     return data_frame
 
 # https://stackoverflow.com/questions/55119651/downsampling-for-more-than-2-classes
