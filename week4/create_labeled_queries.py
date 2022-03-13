b1f08342214ea68b4f7ab21ba38b35b5bfd86448
JT <>
2022-03-13 23:53:52 +0000

    Finish create_labeled_queries.py
    Create a CategoryTree class (in utilities/category_tree.py) that stores
    Category information in a tree hierarchy (and verified that the input
    category information is truly a tree structure in the process).  Have
    create_labeled_queries.py load the categories into that Tree, assign
    queries to categories, compute the sum of queries in each category
    subtree, and then use that to assign each query to the closest ancestor
    category with at least the given minimum number of queries.
    
    NOTE:
    As a check, this checks that there is only one root node, with the
    expected name, and has the expected number of leaf nodes.  The course
    work suggests there are 1540 leaf nodes, but I come up with a different
    answer (3889).  As described in the comments around the test, I found
    out that the 1540 nodes was not counting what I expected it to count.  I
    did confirm I could get the 1540 number, but since I had already written
    up code to count leaf nodes, I kept the check that ensures it is the
    expected value (which I confirmed in other ways should be 3889).  There
    are some other tests I tried when I was trying to figure out what
    computation might get 1540 as well.

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -1,13 +1,20 @@
 import os
+import time
 import argparse
 import xml.etree.ElementTree as ET
 import pandas as pd
-import numpy as np
 import csv
 
 # Useful if you want to perform stemming.
 import nltk
 stemmer = nltk.stem.PorterStemmer()
+
+from collections import defaultdict
+
+from utilities.normalizer import Normalizer
+normalizer = Normalizer(stemmer=stemmer)
+
+from utilities.category_tree import Category, CategoryTree
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
@@ -25,37 +32,209 @@ output_file_name = args.output
 if args.min_queries:
     min_queries = int(args.min_queries)
 
-# The root category, named Best Buy with id cat00000, doesn't have a parent.
-root_category_id = 'cat00000'
-
+print('Parsing category XML {n:s} ...'.format(n=categories_file_name))
 tree = ET.parse(categories_file_name)
 root = tree.getroot()
-
-# Parse the category XML file to map each category id to its parent category id in a dataframe.
-categories = []
-parents = []
+# Parse the category XML file into a CategoryTree.
+categoryTree = CategoryTree()
 for child in root:
-    id = child.find('id').text
-    cat_path = child.find('path')
-    cat_path_ids = [cat.find('id').text for cat in cat_path]
-    leaf_id = cat_path_ids[-1]
-    if leaf_id != root_category_id:
-        categories.append(leaf_id)
-        parents.append(cat_path_ids[-2])
-parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 'parent'])
+    category = Category(ID  =child.find('id'  ).text, \
+                        name=child.find('name').text)
+    cat_path = [Category(ID  =cat.find('id'  ).text, \
+                         name=cat.find('name').text) \
+                for cat in child.find('path')]
+    if (category != cat_path[-1]):
+        raise Exception('ID/name does not match path')
+    categoryTree.add(cat_path)
+categoryDict = categoryTree.categoryDict
+print('  Found {l:d} categories.'.format(l=len(categoryDict)))
 
-# Read the training data into pandas, only keeping queries with non-root categories in our category tree.
+# Check that parsing worked correctly by checking the root nodes and the
+# nubmer of leaf nodes against what the assignment says we should have.
+rootNodes = list()
+leafNodes = list()
+for categoryNode in categoryDict.values():
+    if (categoryNode.parent is None):
+        rootNodes.append(categoryNode)
+    if (0 >= len(categoryNode.childrenDict)):
+        leafNodes.append(categoryNode)
+leafNameSet = set(node.name for node in leafNodes)
+print('Found {l:d} root nodes:'.format(l=len(rootNodes)))
+displayCount = 5
+for categoryNode in sorted(rootNodes, key=lambda node: node.ID):
+    if (0 >= displayCount):
+        print('  ...')
+        break
+    print('  ID: {ID:s}  name: {name:s}'.format(
+          ID  =categoryNode.ID,
+          name=categoryNode.name))
+    displayCount -= 1
+print('Found {l:d} leaf nodes ({n:d} unique names):'.format(
+      l=len(leafNodes), n=len(leafNameSet)))
+displayCount = 2
+for categoryNode in sorted(leafNodes, key=lambda node: node.ID):
+    if (0 >= displayCount):
+        print('  ...')
+        break
+    print('  ID: {ID:s}  name: {name:s}'.format(
+          ID  =categoryNode.ID,
+          name=categoryNode.name))
+    displayCount -= 1
+expectedRootNodes = ['cat00000']
+if (expectedRootNodes != sorted(node.ID for node in rootNodes)):
+    raise Exception('rootNodes not what was expected')
+# NOTE:
+# The coursework says there are 1540 leaf categories (but I now understand
+# that they use ``leaf'' to mean any category, leaf or not, mentioned by a
+# query; I suppose it is a leaf in the sense that it is the end of the
+# category path represented by the query -- the 1540 should be reported by
+# a different line below).
+# I found a different number.  I checked by modifying categoryViewer.py so
+# it counted the number of paths that were NOT substrings of the next path
+# when unique paths were sorted.  I then found out that paths by names were
+# not unique (sometimes there are multiple children categories with
+# different IDs, but the same name), so I also modified categoryViewer.py
+# to print out paths using IDs instead of names.  That produced the same
+# number of category leaves given here:
+expectedLeafNodeLength = 3889
+if (expectedLeafNodeLength != len(leafNodes)):
+    raise Exception('leafNodes was not the expected length')
+
+# Order all of the category nodes by increasing depth.
+# While at it, create a mapping of ID to depth.
+breadthFirstNodes = sorted(rootNodes, key=lambda node: node.ID)
+categoryDepth     = dict()
+ind = 0
+while (ind < len(breadthFirstNodes)):
+    curNode = breadthFirstNodes[ind]
+    parNode = curNode.parent
+    if (parNode is None):
+        categoryDepth[curNode.ID] = 0
+    else:
+        categoryDepth[curNode.ID] = categoryDepth[parNode.ID] + 1
+    childrenNodes = sorted(curNode.childrenDict.values(), \
+                           key=lambda node: node.ID)
+    breadthFirstNodes.extend(childrenNodes)
+    ind += 1
+if (len(breadthFirstNodes) != len(categoryDict)):
+    raise Exception('Not all categories are reachable from root nodes')
+
+# Read the training data into pandas
+print('Reading queries from {n:s} ...'.format(n=queries_file_name))
 df = pd.read_csv(queries_file_name)[['category', 'query']]
-df = df[df['category'].isin(categories)]
+print('Read {l:d} queries with {n:d} distinct categories.'.format(
+      l=len(df), n=len(set(df['category']))))
+df = df[df['category'].isin(categoryDict)]
+print('After filtering to known categories, there are:')
+# This is the line that should report the 1540 categories:
+print('  {l:d} queries with {n:d} distinct categories.'.format(
+      l=len(df), n=len(set(df['category']))))
 
-# IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+# Normalize queries.
+print('Normalizing queries ...')
+normalizedQueryMap = {query:None for query in df['query']}
+print('  Reduced down to {l:d} distinct queries.'.format(
+      l=len(normalizedQueryMap)))
+reportDelta = 4
+nextReport  = time.time() + reportDelta
+for (ind, query) in enumerate(sorted(normalizedQueryMap.keys())):
+    normalizedQueryMap[query] = normalizer.normalize(query)
+    if (time.time() >= nextReport):
+        num = ind + 1
+        den = len(normalizedQueryMap)
+        print('  Current progress:  {n:d} (of {d:d}, {p:5.2f}%)'.format(
+              n=num, d=den, p=100.*num/den))
+        nextReport = time.time() + reportDelta
+df['query'] = [normalizedQueryMap[query] for query in df['query']]
 
-# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+print('Assigning queries to category nodes ...')
+# Start with a map of category ID to the number of queries mapped directly
+# to that ID.
+skipCount          = defaultdict(lambda: 0)
+queryCount         = defaultdict(lambda: 0)
+totalQueriesMapped = 0
+for ID in df['category']:
+    if (ID not in categoryDict):
+        skipCount[ID] += 1
+    queryCount[ID]     += 1
+    totalQueriesMapped += 1
+if (0 < len(skipCount)):
+    print('WARNING:  Skipped {l:d} unrecognized category IDs ({n:d} queries).'.format(
+          l=len(skipCount), n=sum(skipCount.values())))
+if (len(df) - totalQueriesMapped != sum(skipCount.values())):
+    raise Exception('INTERNAL ERROR:  Some queries are unaccounted for')
+if (len(df) != totalQueriesMapped):
+    raise Exception('INTERNAL ERROR:  Pre-filtering of queries missed some unmapped queries')
+# In order of decreasing depth, increase the number of queries associated
+# with the categories by the sum of the counts of all of the immediate
+# children.  Because this is done in order of decreasing depth, this has
+# already been done for all the child nodes by the time we get to the
+# current node, so this will result in each category being associated with
+# the number of queries associated with any category in the subtree rooted
+# at that node.
+for categoryNode in reversed(breadthFirstNodes):
+    for child in categoryNode.childrenDict.values():
+        queryCount[categoryNode.ID] += queryCount[child.ID]
+# Quick check:  The root node should be associated with all the queries.
+if (queryCount[rootNodes[0].ID] != totalQueriesMapped):
+    raise Exception('Not all mapped queries were associated with root node')
+# Create a second map of ID to category.  Each category ID should map to
+# the closest ancestor with at least min_queries queries associated with
+# its subtree.
+# By traversing in order of increasing depth, each element may be mapped to
+# either itself or a value already mapped for its parent, without needing
+# to traverse up to the full depth of the node for each node.
+categoryMap = dict()
+for (unusedDepth, ID) in sorted((categoryDepth[ID], ID) \
+                                for ID in queryCount.keys()):
+    if (queryCount[ID] < min_queries):
+        node = categoryDict[ID]
+        parNode = node.parent
+        if (parNode is None):
+            raise Exception('min_queries is more than the number of queries in the tree')
+        # Because categoryMap is being filled in order of increasing node
+        # depth, the parent should already be mapped.
+        categoryMap[ID] = categoryMap[parNode.ID]
+    else:
+        categoryMap[ID] = ID
+# Roll up the query categories.
+origQueryCategories = set(df['category'])
+df['category'] = [categoryMap[category] for category in df['category']]
+rolledQueryCategories = set(df['category'])
+print('  Assigned {l:d} queries to {r:d} distinct categories (from {o:d}).'.format(
+      l=len(df), r=len(rolledQueryCategories), o=len(origQueryCategories)))
+
+# Here, I was trying to figure out why the course materials say there are
+# 1540 leaf nodes in the category tree.  Try taking just the categories
+# mentioned in queries, and see if there are 1540 leaf nodes there.
+# This does not work.  As mentioned above, the 1540 is the number of
+# distinct categories mentioned by the queries, before filtering to known
+# categories (one of the lines above should report 1540).
+print('Trying a category tree reduced to categories mentioned in queries ...')
+reducedCategoryTree = CategoryTree()
+for (ID, count) in queryCount.items():
+    # During the accumulation procedure, all nodes were added to
+    # queryCount, even the ones not referenced by queries.  Filter those
+    # back out.
+    if (0 >= count):
+        continue
+    reducedCategoryTree.add(categoryTree[ID].path)
+reducedRootNodes = list()
+reducedLeafNodes = list()
+for categoryNode in reducedCategoryTree.categoryDict.values():
+    if (categoryNode.parent is None):
+        reducedRootNodes.append(categoryNode)
+    if (0 >= len(categoryNode.childrenDict)):
+        reducedLeafNodes.append(categoryNode)
+print('  Found {r:d} root nodes and {l:d} leaf nodes ({ld:d} distinct names).'.format(
+      r=len(reducedRootNodes), l=len(reducedLeafNodes),
+      ld=len(set(reducedLeafNodes))))
+print('  Overall, {l:d} categories.'.format(
+      l=len(reducedCategoryTree.categoryDict)))
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
 
-# Output labeled query data as a space-separated file, making sure that every category is in the taxonomy.
-df = df[df['category'].isin(categories)]
+# Output labeled query data as a space-separated file.
 df['output'] = df['label'] + ' ' + df['query']
 df[['output']].to_csv(output_file_name, header=False, sep='|', escapechar='\\', quoting=csv.QUOTE_NONE, index=False)
